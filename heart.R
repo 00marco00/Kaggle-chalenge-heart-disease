a97664f1cdf7c4fd9882a139ca4f6752d596159c
marco-misseri <94753487+00marco00@users.noreply.github.com>
2024-03-05 09:19:53 +0100

    age-sick

--- heart.R
+++ heart.R
@@ -1,4 +1,4 @@
-setwd("C:/Users/marco misseri/OneDrive/Documents/1mam4/R/KKK/Kaggle-chalenge-heart-disease")
+setwd("C:/Users/marco/OneDrive/Documents/GitHub/Kaggle-chalenge-heart-disease")
 data <- read.csv("heart.csv", sep = ",")
 #print( head(data))
 
@@ -15,6 +15,9 @@ pairs(data, main = "Matrice de Scatter Plots")
 
 data_mat <- cor(data)
 #print(data_mat)
+
+
+###################################################################################
 
 counts1 <- table(data$Sex)
 
@@ -48,6 +51,7 @@ barplot(counts,
         col = c("pink", "lightblue"))
 legend("topright", legend = counts, fill = c("pink", "lightblue"))
 
+#####################################################################################
 
 modele_logistique <- glm(HeartDisease ~ Age + Sex + ChestPainType + RestingBP + Cholesterol + FastingBS + RestingECG + MaxHR + ExerciseAngina + Oldpeak + ST_Slope, data = data, family = binomial(link = "logit"))
 #print(summary(modele_logistique))
@@ -64,3 +68,40 @@ res <- table(Predicted = class_pred, Actual = data$HeartDisease)
 #print(res)
 erreur_classification <- sum(res[1,2], res[2,1]) / sum(res)
 print(erreur_classification)
+
+
+
+######################################################################################
+
+# Création de tranches d'âge
+data$AgeGroup <- cut(data$Age, breaks = seq(from = min(data$Age), to = max(data$Age), by = 10))
+
+# Calculer le nombre total de personnes par tranche d'âge
+total_par_age <- table(data$AgeGroup)
+
+# Filtrer pour ne garder que les personnes malades
+data_malade <- subset(data, HeartDisease == 1)
+
+# Calculer le nombre de personnes malades par tranche d'âge
+malades_par_age <- table(data_malade$AgeGroup)
+
+# Calculer le ratio de personnes malades par tranche d'âge pour 100 personnes
+ratio_malades_par_100 <- (malades_par_age / total_par_age) * 100
+
+# Créer un diagramme à barres pour ces ratios
+barplot(ratio_malades_par_100, 
+        main = "Pourcentage de personnes malades par tranche d'âge pour 100 personnes", 
+        xlab = "Tranche d'âge", 
+        ylab = "Pourcentage de malades", 
+        col = "lightblue",
+        las = 2) # Rotation des étiquettes de l'axe des x
+
+# Ajuster un modèle de régression logistique avec la maladie cardiaque en fonction de l'âge
+modele_logistique <- glm(HeartDisease ~ Age, data=data, family=binomial)
+
+# Tracer les données originales (optionnel, pour visualisation)
+plot(data$Age, data$HeartDisease, xlab="Age", ylab="Probabilité de maladie cardiaque", main="Courbe de tendance de maladie cardiaque par âge")
+
+# Tracer la courbe de tendance basée sur le modèle de régression logistique
+curve(predict(modele_logistique, data.frame(Age=x), type="response"), add=TRUE, col="red")
+
